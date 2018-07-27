4a73e923dd446734be8eb745f887bf2cc4ccecc8
Manuel Antonio Ramos Gonzalez <xmannyxfreshx1@gmail.com>
2018-07-27 11:49:02 -0400

    changed favicon and added spinning action to react logo

--- src/Components/CardProfileLinks.js
+++ src/Components/CardProfileLinks.js
@@ -9,7 +9,7 @@ export default class CardProfileLinks extends React.Component {
                     <li><a href='https://www.linkedin.com/in/manuel-ramos-gonzalez-60244010a/'><i className={'fab fa-linkedin'}></i></a></li>
                     <li><a href='https://github.com/xmannyxfreshx1'><i className={'fab fa-github'}></i></a></li>
                     <li><a href='https://medium.com/@xmannyxfreshx1'><i className={'fab fa-medium'}></i></a></li>
-                    <li><a href='https://dev.to/xmannyxfr3shx1'><i className={'fab fa-react'}></i></a></li>
+                    <li><a href='https://dev.to/xmannyxfr3shx1'><i className={'fab fa-react fa-spin'}></i></a></li>
                 </ul>
             </div>
         )
